90439697ee70d2cf6f12da14612aa2da14ba1d0a
Shandiya <141755437+Shandiyaravi@users.noreply.github.com>
2024-01-25 12:58:06 +0100

    update homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from "./components/Homepage";
-import BookPage from "./components/BookPage";
-import CheckoutPage from "./components/CheckoutPage";
+import Homepage from "./pages/Homepage";
+import BookPage from "./pages/BookPage";
+import CheckoutPage from "./pages/CheckoutPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -13,9 +13,9 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path='/' element={<Homepage />} />
-          <Route path='/books/:id' element={<BookPage />} />
-          <Route path='/checkout' element={<CheckoutPage />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/books/:id" element={<BookPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
         </Routes>
         <Footer />
       </Router>
