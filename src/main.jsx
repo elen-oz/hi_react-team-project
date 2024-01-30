672bf2f7661e3c4a673a83fe1f9df10825e800d9
AFGreeneye <afgreeneye@gmail.com>
2024-01-30 11:29:16 +0100

    Added context

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import BookProvider from "./hooks/bookContext.jsx";
+import "./index.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BookProvider>
+      <App />
+    </BookProvider>
   </React.StrictMode>
 );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,42 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import CardItem from "../components/CardItem";
+import { BookContext } from "../hooks/bookContext";
 
 const Homepage = () => {
-  const [books, setBooks] = useState([]);
-  const apiKey = "AIzaSyCf0-8UvYIxpo88-8_sTInFOi-9Hf-Jr4g";
-
-  useEffect(() => {
-    const fetchBooks = async () => {
-      try {
-        const response = await fetch(
-          `https://www.googleapis.com/books/v1/volumes?q=*&maxResults=30&key=${apiKey}`
-        );
-
-        const data = await response.json();
-
-        if (data.items) {
-          // Filter books that have imageLinks.thumbnail
-          const filteredBooks = data.items.filter(
-            (book) =>
-              book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail
-          );
-
-          setBooks(filteredBooks);
-        } else {
-          console.error("No books found in the response");
-        }
-      } catch (error) {
-        console.error("Error fetching books:", error.message);
-      }
-    };
-    fetchBooks();
-  }, []);
-
-  return (
-    <div>
-      <CardItem books={books} />
-    </div>
-  );
+  const { books } = useContext(BookContext);
+  console.log(books);
+  return books.map((book) => {
+    return (
+      <CardItem
+        image={book.volumeInfo.imageLinks.thumbnail}
+        title={book.volumeInfo.title}
+      />
+    );
+  });
 };
 
 export default Homepage;
