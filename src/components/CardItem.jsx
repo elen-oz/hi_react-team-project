36fcf0ce0b927c19fe705e21d1bdf7c0b4daaabb
jesslee2023 <jingping.li@hyperisland.se>
2024-01-30 21:07:05 +0100

    add review

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,4 +1,6 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import ReviewModal from "./ReviewModal";
 
 function makeShorterName(name) {
   let nameArray = name;
@@ -10,13 +12,19 @@ function makeShorterName(name) {
 }
 
 const CardItem = ({ title, image, id }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = (e) => {
+    // e.stopPropagation();
+    setShowModal(!showModal);
+  };
   return (
     <div key={id} className='col '>
-      <Link to={`/books/${id}`}>
-        <div className='card'>
+      <div className='card align-items-center'>
+        <Link to={`/books/${id}`}>
           <div
             className='w-100 overflow-hidden d-flex align-items-center'
-            style={{ height: '250px' }}
+            style={{ height: "250px" }}
           >
             <img
               src={image}
@@ -27,10 +35,27 @@ const CardItem = ({ title, image, id }) => {
 
           <div className='card-body'>
             <h6 className='card-title fs-6'>{makeShorterName(title)}</h6>
-            <button className='btn btn-primary'>Add to Cart</button>
           </div>
+        </Link>
+
+        <div className='d-flex gap-2 mb-3'>
+          <button className='btn btn-primary'>Add to Cart</button>
+          <button
+            className='btn btn-info'
+            data-bs-toggle='modal'
+            data-bs-target={`#reviewModal${id}`}
+            onClick={toggleModal}
+          >
+            Review
+          </button>
+          <ReviewModal
+            id={id}
+            title={title}
+            show={showModal}
+            handleClose={toggleModal}
+          />
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
---
+++ src/components/ReviewModal.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import Rating from "react-rating-stars-component";
+
+const ReviewModal = ({ id, title, show, handleClose }) => {
+  const [rating, setRating] = useState(0);
+  const handleRatingChange = (newValue) => {
+    setRating(newValue);
+  };
+  const handleSubmit = () => {
+    // console.log("Rating:", rating);
+    const message = document.getElementById(`reviewModalMessage${id}`).value;
+
+    const reviewData = {
+      rating: rating,
+      message: message,
+    };
+
+    const reviewDataJson = JSON.stringify(reviewData);
+
+    localStorage.setItem(`reviewData${id}`, reviewDataJson);
+    handleClose();
+  };
+  return (
+    <div
+      className={`modal fade ${show ? "show" : ""}`}
+      id={`reviewModal${id}`}
+      tabIndex='-1'
+      aria-labelledby={`modalLabel${id}`}
+      aria-hidden={!show}
+      style={{ display: show ? "block" : "none" }}
+      // data-bs-backdrop='static'
+    >
+      <div className='modal-dialog'>
+        <div className='modal-content'>
+          <div className='modal-header'>
+            <h5 className='modal-title' id={`modalLabel${id}`}>
+              Review for {title}{" "}
+            </h5>
+          </div>
+          <div className='modal-header'>
+            <h6>
+              Author:
+              {/* {item.author} */}
+            </h6>
+          </div>
+          <div className='modal-body'>
+            <div className='form-group'>
+              <label>Rating:</label>
+              <Rating value={rating} onChange={handleRatingChange} />
+            </div>
+            <div className='form-group'>
+              <label htmlFor={`reviewModalMessage${id}`}>Message:</label>
+              <textarea
+                className='form-control'
+                id={`reviewModalMessage${id}`}
+                placeholder='Book review'
+              ></textarea>
+            </div>
+          </div>
+
+          <div className='modal-footer'>
+            <button
+              type='button'
+              className='btn btn-secondary'
+              data-bs-dismiss='modal'
+              onClick={handleClose}
+            >
+              Close
+            </button>
+            <button
+              type='button'
+              className='btn btn-primary'
+              data-bs-dismiss='modal'
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewModal;
