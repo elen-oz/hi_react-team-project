9afdc7c3489abacd504e92133805e82fabc8b350
Shandiya <141755437+Shandiyaravi@users.noreply.github.com>
2024-01-31 23:21:21 +0100

    updated bookpage

---
+++ src/hooks/bookDetailsContext.jsx
@@ -0,0 +1,38 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+export const BookDetailsContext = createContext();
+
+const BookDetailsProvider = ({ children }) => {
+  const { id } = useParams();
+  const [bookDetails, setBookDetails] = useState(null);
+
+  useEffect(() => {
+    const fetchBookDetails = async () => {
+      try {
+        const response = await fetch(
+          `https://www.googleapis.com/books/v1/volumes/${id}`
+        );
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch book details');
+        }
+
+        const data = await response.json();
+        setBookDetails(data);
+      } catch (error) {
+        console.error('Error fetching book details:', error);
+      }
+    };
+
+    fetchBookDetails();
+  }, [id]);
+
+  return (
+    <BookDetailsContext.Provider value={{ bookDetails }}>
+      {children}
+    </BookDetailsContext.Provider>
+  );
+};
+
+export default BookDetailsProvider;
--- src/pages/BookPage.jsx
+++ src/pages/BookPage.jsx
@@ -1,60 +1,47 @@
+import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { BookDetailsContext } from '../hooks/bookDetailsContext';
 import RatingStars from '../components/RatingStars';
 
 const BookPage = () => {
-  const { id } = useParams();
+  const { bookDetails } = useContext(BookDetailsContext);
 
-  const [bookDetails, setBookDetails] = useState(null);
   const [storedRating, setStoredRating] = useState(null);
 
-  useEffect(() => {
-    const fetchBookDetails = async () => {
-      try {
-        const response = await fetch(
-          `https://www.googleapis.com/books/v1/volumes/${id}`
-        );
-        const data = await response.json();
-
-        setBookDetails(data);
-      } catch (error) {
-        console.error('Error fetching book details:', error);
-      }
-    };
-
-    fetchBookDetails();
-
-    const storedRatingData = localStorage.getItem(`reviewData${id}`);
-    if (storedRatingData) {
-      const parsedData = JSON.parse(storedRatingData);
-      setStoredRating(parsedData.rating);
-    }
-  }, [id]);
+  // useEffect(() => {
+  //   const storedRatingData = localStorage.getItem(`reviewData${id}`);
+  //   if (storedRatingData) {
+  //     const parsedData = JSON.parse(storedRatingData);
+  //     setStoredRating(parsedData.rating);
+  //   }
+  // }, [bookDetails]);
 
   return (
-    <div className='container'>
-      {bookDetails ? (
-        <div className='card' style={{ width: '18rem' }}>
-          <img
-            src={bookDetails.volumeInfo.imageLinks.thumbnail}
-            className='card-img-top'
-            alt={bookDetails.volumeInfo.title}
-          />
-
-          <div className='card-body'>
-            <h5 className='card-title'>{bookDetails.volumeInfo.title}</h5>
-            {storedRating ? (
-              <RatingStars value={storedRating} edit={false} />
-            ) : (
-              <RatingStars value={0} edit={false} />
-            )}
-            <p className='card-text'>{bookDetails.volumeInfo.description}</p>
-            <button className='btn btn-primary'>More Info</button>
-          </div>
-        </div>
-      ) : (
-        <div>Loading...</div>
-      )}
+    <div className='card mb-3 m-4' style={{ maxWidth: '540px' }}>
+      <div className='row g-2 '>
+        {bookDetails ? (
+          <>
+            <div className='col-md-4'>
+              <img
+                src={bookDetails.volumeInfo.imageLinks.thumbnail}
+                className='img-fluid rounded-start'
+                alt={bookDetails.volumeInfo.title}
+                style={{ padding: '1rem' }}
+              />
+            </div>
+            <div className='col-md-8'>
+              <div className='card-body'>
+                <h5 className='card-title'>{bookDetails.volumeInfo.title}</h5>
+                <p className='card-text'>
+                  {bookDetails.volumeInfo.description}
+                </p>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
     </div>
   );
 };
