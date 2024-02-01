180134183a46e861fcbef00e385a23aa9d37c0c4
elen-oz <ozerova.lenka@gmail.com>
2024-02-01 14:31:05 +0100

    change layout

--- src/components/CartPopUp.jsx
+++ src/components/CartPopUp.jsx
@@ -18,7 +18,7 @@ const CartPopUp = ({ isOpen, closeCart }) => {
   const { darkMode } = useContext(ThemeContext);
 
   const bgColorClass = darkMode ? 'bg-dark' : 'bg-light';
-  const textColorClass = darkMode ? 'bg-dark' : 'bg-light';
+  const textColorClass = darkMode ? 'text-light' : 'text-dark';
 
   return (
     <div
@@ -31,46 +31,43 @@ const CartPopUp = ({ isOpen, closeCart }) => {
         className='container my-4 p-4  mt-0bg-light border'
         style={{ width: '30rem', overflowY: 'auto' }}
       >
-        {/* {darkMode ? (
-          <h5 className='text-center mt-3 text-white'>My Book Cart</h5>
-        ) : ( */}
-        <h5 className={`text-center mt-3 ${textColorClass}`}> My Book Cart</h5>
-        {/* )} */}
+        <div className='d-flex justify-content-between align-items-center'>
+          <h5 className={`text-center mt-3 ${textColorClass}`}>
+            {' '}
+            My Book Cart
+          </h5>
+          <button className='btn btn-danger' onClick={clearCart}>
+            Clear
+          </button>
+        </div>
         <hr />
 
-        {/* <ListGroup>
+        <ul className={`${textColorClass}`}>
           {items.map((item, index) => (
-            <ListGroup.Item key={index}>
-              {item.title} - {item.author}
-            </ListGroup.Item>
-          ))}
-        </ListGroup> */}
-
-        <ul>
-          {items.map((item, index) => (
-            <li key={index}>
+            <li
+              key={index}
+              className='d-flex justify-content-between align-items-center'
+            >
               {item.volumeInfo.title} - {item.volumeInfo.authors}{' '}
               <button
-                className='btn btn-warning'
+                className='btn btn-warning m-1 py-1'
                 onClick={() => {
                   handleRemoveItemToCart(item.id);
                 }}
               >
-                Remove
+                X
               </button>
             </li>
           ))}
         </ul>
 
-        <div>Total: {totalAmount}</div>
+        <div className={`${textColorClass}`}>Total: {totalAmount}</div>
 
         <div className='d-flex justify-content-between mt-3'>
           <Link to='/checkout' className='btn btn-success'>
             CHECKOUT
           </Link>
-          <button className='btn btn-danger' onClick={clearCart}>
-            Clear
-          </button>
+
           <button className='btn btn-secondary' onClick={closeCart}>
             Close Cart
           </button>
