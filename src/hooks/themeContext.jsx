17955c8a536f25b82002eef7c844d82bcc3f5255
elen-oz <ozerova.lenka@gmail.com>
2024-02-01 21:17:48 +0100

    fix err

--- src/hooks/themeContext.jsx
+++ src/hooks/themeContext.jsx
@@ -1,18 +1,19 @@
 import { createContext, useEffect, useState } from 'react';
+
 export const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
   const [darkMode, setDarkMode] = useState(false);
-  const bgColorClass = darkMode ? 'bg-dark' : 'bg-light';
 
   useEffect(() => {
-    if (window.matchMedia) {
+    const localStorageDarkMode = localStorage.getItem('darkMode');
+    if (localStorageDarkMode) {
+      setDarkMode(JSON.parse(localStorageDarkMode));
+    } else if (window.matchMedia) {
       let darkThemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
       if (darkThemeQuery.matches) {
         setDarkMode(true);
-      } else {
-        setDarkMode(false);
       }
     }
   }, []);
@@ -21,6 +22,8 @@ const ThemeProvider = ({ children }) => {
     setDarkMode(!darkMode);
     localStorage.setItem('darkMode', !darkMode);
   };
+
+  const bgColorClass = darkMode ? 'bg-dark' : 'bg-light';
 
   return (
     <ThemeContext.Provider value={{ darkMode, darkModeHandle, bgColorClass }}>
