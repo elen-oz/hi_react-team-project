52549fda2991508d7e92333c3ddb058ea7e17bec
elen-oz <ozerova.lenka@gmail.com>
2024-02-05 18:02:41 +0100

    refactor

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { Rating } from '@smastrom/react-rating';
 import { makeShorterName } from '../utils/utils';
 import styles from '../index.module.css';
--- src/hooks/bookDetailsContext.jsx
+++ src/hooks/bookDetailsContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export const BookDetailsContext = createContext();
