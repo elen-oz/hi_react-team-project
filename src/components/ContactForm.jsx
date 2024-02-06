991cdef353415460cc28f1ac394e869d9d92d4d2
elen-oz <ozerova.lenka@gmail.com>
2024-02-06 10:03:44 +0100

    fix

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -17,7 +17,7 @@ const validationSchema = Yup.object().shape({
   message: Yup.string().required('*Message required'),
 });
 
-const CheckoutForm = () => {
+const ContactForm = () => {
   const { bgColorClass, textColorClass } = useContext(ThemeContext);
 
   return (
