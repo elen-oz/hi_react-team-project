31615e853fc0b1727656206005af5bd927ebe55f
elen-oz <ozerova.lenka@gmail.com>
2024-02-06 10:19:54 +0100

    fix: display lists

--- src/components/CheckoutListToLoans.jsx
+++ src/components/CheckoutListToLoans.jsx
@@ -20,9 +20,9 @@ const CheckoutListToLoans = () => {
         {loanedItems.map((item) => (
           <li
             key={item.id}
-            className='list-group-item d-flex justify-content-between align-items-center'
+            className='list-group-item d-flex justify-content-between align-items-center  pe-1'
           >
-            <span className='w-100 px-2 d-flex justify-content-between align-items-center'>
+            <span className='w-100 ps-2 d-flex justify-content-between align-items-center'>
               <span>{item.volumeInfo.title}</span>
 
               <button
